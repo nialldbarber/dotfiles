c53c276cf0dd4ac915775e5abe837f4f6b33bd03
Niall Barber <niall.barber@MBP-0093.local>
2019-08-05 11:03:51 +0100

    Initial commit

---
+++ .vscode-settings.js
@@ -0,0 +1,47 @@
+{
+  // Window 
+  "window.title": "${dirty} ${activeEditorMedium}${separator}${rootName}",
+  // Workbench
+  "workbench.colorTheme": "Subliminal Next",
+  "workbench.editor.tabSizing": "shrink",
+  "workbench.sideBar.location": "left",
+  // Editor 
+  "editor.fontFamily": "Operator Mono, Menlo, Monaco, 'Courier New', monospace",
+  "editor.fontSize": 16,
+  "editor.fontWeight": "300",
+  "editor.lineHeight": 24,
+  "editor.letterSpacing": 0.5,
+  "editor.tabSize": 2,
+  "editor.cursorWidth": 2,
+  "editor.cursorStyle": "line",
+  "editor.wordWrap": "on",
+  "editor.cursorBlinking": "solid",
+  "editor.colorDecorators": false,
+  "editor.minimap.enabled": false,
+  "editor.renderWhitespace": "none",
+  "editor.rulers": [
+    80,
+    100
+  ],
+  "editor.minimap.showSlider": "always",
+  "editor.snippetSuggestions": "top",
+  "editor.formatOnSave": true,
+  "editor.autoIndent": true,
+  "editor.matchBrackets": true,
+  "editor.useTabStops": true,
+  // Emmet
+  "emmet.triggerExpansionOnTab": true,
+  "emmet.includeLanguages": {
+    "javascript": "javascriptreact",
+  },
+  // Files 
+  "files.insertFinalNewline": true,
+  "files.associations": {
+    "*.js": "javascriptreact"
+  },
+  // Prettier 
+  "prettier.singleQuote": true,
+  "prettier.arrowParens": "always",
+  "prettier.endOfLine": "lf",
+}
+
